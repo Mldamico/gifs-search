5803957a2f944b63cf449eebfb571c8751e16e52
mldamico <matias.lioneldamico@gmail.com>
2023-06-01 20:34:14 -0300

    Save and load in localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private _tagsHistory: string[] = [];
   private apiKey: string = environment.API_KEY;
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -26,6 +28,18 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage() {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage() {
+    if (!localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
   }
 
   public searchTag(tag: string) {
